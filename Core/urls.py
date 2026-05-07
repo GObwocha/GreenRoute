5f01e14b9b07b03d2b796e825b3a65194cefb7b4
Benson Mose <mosebemos26@gmail.com>
2026-05-07 11:33:42 +0300

    added urls.py on main for easier routing, all the urls will live here and i had regested it in core urls

--- Core/urls.py
+++ Core/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include #added include to the path import to allow us to import urls from other apps.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('main.urls')), #this will include the urls from the main app 
 ]
