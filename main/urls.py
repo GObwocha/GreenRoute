e04a1f3b3e1877c0d69f9f42d930ec90346c4042
Benson Mose <mosebemos26@gmail.com>
2026-05-14 10:31:50 +0300

    Connect admin-panel route

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/route/', views.calculate_route, name='calculate_route'),
+    path('admin-panel/', views.admin_panel, name='admin_panel'),
 ]
